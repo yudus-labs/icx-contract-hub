d3a5699262acf594cf15a496869726bd694a9fe0
duyyudus <duyyudus@gmail.com>
2020-06-20 13:11:45 +0700

    Clean unused test

--- src/IcxExplorer.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import IcxExplorer from './IcxExplorer';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<IcxExplorer />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
